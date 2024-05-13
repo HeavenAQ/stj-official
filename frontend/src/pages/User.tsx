b0811ce7f00f7d9efd6f23d28f590955cbe2284e
Heaven Chen <891118heaven@gmail.com>
2024-05-13 19:22:14 +0800

    [Update] if user is not logged in, redirect to login page

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,19 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { getUser } from "../api/user";
 
 const User = () => {
+  const navigate = useNavigate();
   const { data: user } = useQuery({
     queryKey: ["user"],
     queryFn: () => getUser(),
   });
 
+  if (user === undefined) {
+    navigate("/login");
+  }
+
   return (
     <div className="mt-20 w-full h-11 text-center bg-slate-300">
-      {user === undefined ? (
-        <a href="/login">Login First</a>
-      ) : (
-        <h1>{user?.data.email}</h1>
-      )}
+      <h1>{user?.data.email}</h1>
     </div>
   );
 };
